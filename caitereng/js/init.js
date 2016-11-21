dd0699e0d3e140deabbb25daffef004890965be9
AnastasiyaVN14 <AnastasiyaVN14@gmail.com>
2016-11-21 16:12:25 +0300

    Модульное окно для кейтеринг, Реалвуд-бутстрап

---
+++ caitereng/js/init.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+;(function ($){
+    $(window).on('load', function(){
+        //init modal
+        $('._order-modal-open').on('click', function (){
+            $('#order').bPopup({
+                closeClass: 'close',
+            });
+        });
+       //formstyler
+        $('select').styler({
+        	selectSmartPositioning: false
+        });
+
+        //slick nav
+        $('.header-nav').slicknav({
+        	prependTo:'.header .container',
+        	label:'',
+        });
+    });
+    
+})(jQuery)
+// ;(functio )
+
+
+// //init modal
+// $('._order-modal-open').on('click', function (){
+//         $('#order').bPopup({
+//             closeClass: 'close',
+//         });
+// });
+
+// // formstyler
+// $('select').styler({
+// 	selectSmartPositioning: false
+// });
+// //slick nav
+// $('.header-nav').slicknav();
+
+// ;(function($){
+
+// })(jQuery)
